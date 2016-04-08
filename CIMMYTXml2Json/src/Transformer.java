0cba43452f3bdb653933e95193e2f75c7596ef7f
mihalispap <mihalispapak@gmail.com>
2016-04-08 12:00:17 +0300

    init

---
+++ CIMMYTXml2Json/src/Transformer.java
@@ -0,0 +1,14 @@
+
+public class Transformer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        if (args.length != 2) {
+            System.err.println("Usage: param1(inputdir) param2(outputdir)");                
+            System.exit(1);
+        } 
+        
+	}
+
+}
